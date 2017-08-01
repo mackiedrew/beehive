eec25038ee9e67ab3db5e194b923595f6a25f46c
Mackie Drew <mackie@shopbonsai.ca>
2017-07-31 21:24:51 -0400

    :sparkles: Added flowtype coverage.

--- imports/ui/containers/App.jsx
+++ imports/ui/containers/App.jsx
@@ -1,9 +1,17 @@
-// Framework
-import React from 'react';
+// @flow
 
-const App = () =>
+// Framework
+import type React, { Element } from 'react';
+
+/**
+ * Main rendering entry point for the app.
+ *
+ * @returns {ReactElement} Stateless functional React component.
+ */
+const App = (): Element<*> => (
   <div className="app">
     Test 123
-  </div>;
+  </div>
+);
 
 export default App;
