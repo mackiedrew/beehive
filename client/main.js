e82765e75029b71283cb7afc515831964a87550d
Mackie Drew <mackie@shopbonsai.ca>
2017-07-31 22:08:48 -0400

    :sparkles: Added LESS, bootstrap 4, reactstrap

--- client/main.js
+++ client/main.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
+// Libraries
+import 'bootstrap/dist/css/bootstrap.css';
+
 // Components
 import App from '../imports/ui/containers/App.jsx';
 
--- imports/ui/containers/App.jsx
+++ imports/ui/containers/App.jsx
@@ -3,15 +3,38 @@
 // Framework
 import type React, { Element } from 'react';
 
+// Components
+import { Container, Row, Col } from 'reactstrap';
+
 /**
  * Main rendering entry point for the app.
  *
- * @returns {ReactElement} Stateless functional React component.
+ * @returns {Element} Stateless functional React component.
  */
 const App = (): Element<*> => (
-  <div className="app">
-    Test 123
-  </div>
+  <Container className="app px-0" fluid>
+    <Row className="bg-faded">
+      <Col>
+        <header className="py-4 text-center">
+          <h1>Beehive Simulator</h1>
+        </header>
+      </Col>
+    </Row>
+    <Row noGutters>
+      <Col>
+        <main className="bg-success">
+          <p>Main!</p>
+        </main>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <footer className="py-1 text-center">
+          <p>Created by: <a href="https://mackiedrew.com/">Mackie Drew</a></p>
+        </footer>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default App;
